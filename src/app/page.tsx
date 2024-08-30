ee3982b0193d896daa5e12e3470fe055e5f3bd28
britta oblan <brittaoblan@gmail.com>
2024-08-30 23:13:22 +0800

    UI screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
-import Image from "next/image";
-import ScreenStepper from "@/components/Stepper";
+"use client"
+
 import MealForm from "@/components/MealForm";
 import { bangers } from "./layout";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  const [sectionTitle, setSectionTitle] = useState('');
+
   return (
-    <main className="flex min-h-screen flex-col">
-      <div className="bg-lime-500 items-center justify-between p-12 text-center">
-        <h1 className={`text-white ${bangers.className}`}>buzz eats</h1>
+    <main className="min-h-screen items-center flex-col justify-center">
+      <div className="bg-lime-500 p-12 flex justify-center">
+        <div className="max-w-5xl items-center text-center">
+          <h1 className={`text-white text-5xl p-5 ${bangers.className}`}>buzz eats</h1>
+          {sectionTitle === '' && (<p className="text-white p-5"><strong>Ola!</strong>  I'm an AI-Powered meal generator to help you create quick meals based on your preference such as allergens, diet restrictions, your calorie intake or even whatever is available in your kitchen. Click below to <strong>start.</strong></p>)}
+          <h3 className="text-white p-5 font-bold">{sectionTitle}</h3>
+        </div>
       </div>
-      <div className="items-center justify-between p-24 text-center">
-        <MealForm></MealForm>
+      <div className="flex justify-center">
+        <div className="max-w-5xl flex items-center text-center min-h-80 py-24 px-12 ">
+          <div className="w-full">
+            <MealForm activeTitle={setSectionTitle}></MealForm>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="max-w-5xl w-full items-center flex justify-center border-solid border-t border-stone-200 mx-12 p-4">
+          <p className="text-xs text-stone-400 text-center">Created by <Link href="https://britzoblan.com">Britta Oblan</Link></p>
+        </div>
       </div>
     </main>
   );
---
+++ src/config/constants.ts
@@ -0,0 +1,52 @@
+const sectionTitle = [
+    "",
+    "I need help with my meal and…",
+    "My kitchen has...",
+    "I need a meal with estimated calorie of...",
+    "My daily calorie intake is...",
+    "I want to make meals for...",
+    "I have diet restrictions or allergic to...",
+    "I want a meal",
+]
+
+
+const allergenItems = [
+    {
+        id: 'fish',
+        label: 'Fish'
+    },
+    {
+        id: 'pork',
+        label: 'Pork'
+    },
+    {
+        id: 'beef',
+        label: 'Beef'
+    },
+    {
+        id: 'shellfish',
+        label: 'Shellfish / Crustaceans'
+    },
+    {
+        id: 'molluscs',
+        label: 'Molluscs'
+    },
+    {
+        id: 'eggs',
+        label: 'Eggs'
+    },
+    {
+        id: 'gluten',
+        label: 'Gluten'
+    },
+    {
+        id: 'treenuts',
+        label: 'Tree Nuts'
+    },
+]
+
+
+export {
+    sectionTitle,
+    allergenItems
+}
