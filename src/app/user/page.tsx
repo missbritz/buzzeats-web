e1b2898aee093bbb93616cfc839f20ad2ddb3f91
britta oblan <brittaoblan@gmail.com>
2024-10-06 11:03:43 +0800

    Meal list

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,7 +2,7 @@
 
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import UserMeals from '@/components/UserMeals';
 
 const allMeals = [
     {
@@ -26,29 +26,7 @@ export default function Home() {
     return (
         <main className="min-h-screen items-center flex-col justify-center">
             <Header />
-            <div className="flex justify-center">
-                <div className="max-w-5xl w-full self-center py-12">
-                    <div className="w-full px-5">
-                        <h2>Hi <strong>User</strong></h2>
-                        <Table className="w-full">
-                            <TableHeader>
-                                <TableRow>
-                                <TableHead className="w-[100px] text-left">Date</TableHead>
-                                <TableHead className="text-left">Meal Name</TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {allMeals.map((meal) => (
-                                <TableRow key={meal.mealId}>
-                                    <TableCell className="font-medium text-left">{meal.mealDate}</TableCell>
-                                    <TableCell className="text-left">{meal.mealName}</TableCell>
-                                </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </div>
-                </div>
-            </div>
+                <UserMeals savedMeals={allMeals}/>
             <Footer/>
         </main>
     );
---
+++ src/components/UserMeals.tsx
@@ -0,0 +1,38 @@
+import { Table } from "lucide-react"
+import { TableHeader, TableRow, TableHead, TableBody, TableCell } from "./ui/table"
+
+
+const UserMeals = ({ savedMeals }: any) => {
+    return (
+        <div className="flex justify-center">
+            <div className="max-w-5xl w-full self-center py-12">
+                <div className="w-full px-5">
+                    <h2>Hi <strong>User</strong></h2>
+                    {savedMeals.length ? (
+                    <Table className="w-full">
+                        <TableHeader>
+                            <TableRow>
+                            <TableHead className="w-[100px] text-left">Date</TableHead>
+                            <TableHead className="text-left">Meal Name</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {savedMeals.map((meal) => (
+                            <TableRow key={meal.mealId}>
+                                <TableCell className="font-medium text-left">{meal.mealDate}</TableCell>
+                                <TableCell className="text-left">{meal.mealName}</TableCell>
+                            </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                    ) : (
+                        <p>You have no saved meals yet.</p>
+                    )}
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserMeals
