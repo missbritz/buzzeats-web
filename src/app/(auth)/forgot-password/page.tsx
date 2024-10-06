b50c88a1628bbc012c934564d4c6ac2ca62a25ea
britta oblan <brittaoblan@gmail.com>
2024-10-06 13:55:20 +0800

    Add forgot pw

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,27 +1,15 @@
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+'use client';
+
+import ForgotPasswordForm from "@/components/ForgotPasswordForm";
+import { useSearchParams } from "next/navigation";
 
 export default function ForgotPasswordPage() {
+  const searchParams = useSearchParams();
+  const param = {};
+  param['error'] = searchParams.get('error');
+  param['success'] = searchParams.get('success');
+  param['message'] = searchParams.get('message');
   return (
-      <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
-        <div>
-          <h1 className="text-2xl font-medium">Reset Password</h1>
-          <p className="text-sm text-secondary-foreground">
-            Already have an account?{" "}
-            <Link className="text-primary underline" href="/sign-in">
-              Sign in
-            </Link>
-          </p>
-        </div>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <Button>
-            Reset Password
-          </Button>
-        </div>
-      </form>
+      <ForgotPasswordForm searchParams={param} />
   );
 }
