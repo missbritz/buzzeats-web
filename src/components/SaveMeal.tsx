209c30004facfb09936e016e1c451f95fc813178
britta oblan <brittaoblan@gmail.com>
2024-10-16 21:28:08 +0800

    Update save

---
+++ src/components/SaveMeal.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { MealTypeDef, MessageDef } from './Meal';
+import { Button } from "./ui/button"
+import { saveMeal } from '@/actions/tables';
+
+export default function SaveMeal ({ user, meal}: { user: any, meal: MealTypeDef }) {
+    const [message, setMessage] = useState<MessageDef>(''); 
+
+    const SaveMealFn = (meal:MealTypeDef, userId: string) => {
+        const saveMealData = saveMeal(meal, userId);
+        setMessage(saveMealData)
+    }
+    
+    return (
+        <div>
+        { user ? (
+            <div>
+            <Button onClick={() => SaveMealFn(meal, user.user_metadata.sub)}>Save this meal</Button>
+                {message}
+            </div>
+            ) : <></>
+        }
+        </div>
+    )
+}
