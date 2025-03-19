caa993d2078930dfd9e9c7f386fe7bf585ba4e85
britta oblan <brittaoblan@gmail.com>
2025-03-19 13:35:54 +1100

    Reorder fields

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -18,10 +18,10 @@ const RegisterForm = ({ message }: { message: Message }) => {
           </Link>
         </p>
         <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
           <Label htmlFor="name">Name</Label>
           <Input name="name" placeholder="John Smith" required/>
+          <Label htmlFor="email">Email</Label>
+          <Input name="email" placeholder="you@example.com" required />
           <Label htmlFor="password">Password</Label>
           <Input
             type="password"
